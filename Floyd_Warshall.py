f7dc258632a8385bf57c24144e4147a75c00a826
irynadosiak <43314418+irynadosiak@users.noreply.github.com>
2021-01-16 22:26:46 +0200

    Create Floyd Warshall

---
+++ Floyd_Warshall.py
@@ -0,0 +1,11 @@
+def floyd_warshall(graph, N):
+    distance = list(map(lambda i: list(map(lambda j: j, i)), graph))
+    for k in range(N):
+        for i in range(N):
+            for j in range(N):
+                distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
+    return distance
+
+
+matrix = [[0, 8, 2], [8, 0, 1], [2, 1, 0]]
+print(floyd_warshall(matrix, 3))
