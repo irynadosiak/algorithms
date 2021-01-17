ab005f6caa9277e84ff2d1aeee506414868e62e8
irynadosiak <43314418+irynadosiak@users.noreply.github.com>
2021-01-17 11:23:57 +0200

    Create linear search

--- linearSearch.py
+++ linearSearch.py
@@ -1,4 +1,4 @@
-def linear_search(array, n, x):
+def linearSearch(array, n, x):
     for i in range(0, n):
         if (array[i] == x):
             return i
@@ -8,7 +8,7 @@ def linear_search(array, n, x):
 array = [2, 3, 4, 28, 50]
 x = 28
 n = len(array)
-if linear_search(array, n, x) == -1:
+if linearSearch(array, n, x) == -1:
     print("Element is not present in array")
 else:
-    print("Element is present at index", linear_search(array, n, x))
+    print("Element is present at index", linearSearch(array, n, x))
